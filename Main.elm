0f6d91ff98e5e38ae45cd4b61ebb20118a69e524
newfivefour <newfivefour@newfivefour.com>
2016-11-13 22:24:48 +0100

    Front page fuckery.

--- Main.elm
+++ Main.elm
@@ -59,27 +59,29 @@ pageUpdate msg model =
 view : Model -> Html Msg
 view model =
   let
-    loginVis   = if model.page == "login" then "block" else "none"
-    homeVis    = if model.page == "home" then "block" else "none"
-    addVis     = if model.page == "add" then "block" else "none"
-    pointy     = style [("cursor", "pointer")]
-    floatLeft  = style [("float", "left"), ("margin-right", "10px")]
+    loginVis    = if model.page == "login" then "block" else "none"
+    homeVis     = if model.page == "home" then "block" else "none"
+    addVis      = if model.page == "add" then "block" else "none"
+    pointy      = style [("cursor", "pointer")]
+    floatLeft   = style [("float", "left"), ("margin-right", "10px")]
     --companies  = div [] [renderCompany model.companies, map]
-    companies  = div [] [map]
-    map        = div [id "mapid", floatLeft ] []
-    login      = div [] [renderLogin model.session model.loginInput]
-    nav        = div [] [
-      span [pointy, onClick (HomePage |> Pages)] [text "home"]
+    companyInfo = div [ floatLeft ] [
+      div [] [text "Name"]
+      , div [] [text "Technologies"]
+    ]
+    companies   = div [] [ map, companyInfo ]
+    map         = div [id "mapid", floatLeft ] []
+    login       = div [] [renderLogin model.session model.loginInput]
+    nav         = div [] [
+      span [pointy, onClick (HomePage |> Pages)] [text "list"]
       , span [] [text " | "]
-      , span [pointy, onClick (LoginPage |> Pages)] [text "login"]
-      , span [] [text " | "]
-      , span [pointy, onClick (CompanyAddPage |> Pages)] [text "add"]
+      , span [pointy, onClick (CompanyAddPage |> Pages)] [text "+"]
     ]
   in
     div []
         [ nav
         , div [ style [ ("display", loginVis) ] ] [ login ]
-        , div [ style [ ("display", addVis) ] ] [ renderCompanyAdd ]
+        , div [ style [ ("display", addVis) ] ] [ login, renderCompanyAdd model]
         , div [ style [ ("display", homeVis) ] ] [ companies ]
         ]
 
--- Views/CompanyAdd.elm
+++ Views/CompanyAdd.elm
@@ -1,25 +1,28 @@
 module Views.CompanyAdd exposing (renderCompanyAdd)
 
-import Html exposing (Html, div, text, button, input)
+import Html exposing (Html, div, text, button, input, span)
 import Html.Events exposing (onClick, onInput)
 import Html.Attributes exposing (placeholder, type')
 import Model exposing (Model, LoginInputModel)
 import Messages exposing (Msg(CompanyAdd), CompanyInputData(..))
 
-renderCompanyAdd: Html Msg
-renderCompanyAdd =
-  div [] [
-      div []
-          [
-            input [ onInput (Name >> CompanyAdd), type' "text", placeholder "name"] []
-          ]
-      , div []
-          [
-            input [ onInput (Lat >> CompanyAdd), type' "text", placeholder "lat" ] []
-         ]
-      , div []
-          [
-            input [ onInput (Lon >> CompanyAdd), type' "text", placeholder "lon" ] []
-         ]
-      , button [ onClick (CompanyAddPress |> CompanyAdd), type' "text" ] [ text "Add company" ]
-      ]
+renderCompanyAdd : Model -> Html Msg
+renderCompanyAdd model =
+  if model.session == "" then
+    span [] []
+  else
+    div [] [
+        div []
+            [
+              input [ onInput (Name >> CompanyAdd), type' "text", placeholder "name"] []
+            ]
+        , div []
+            [
+              input [ onInput (Lat >> CompanyAdd), type' "text", placeholder "lat" ] []
+           ]
+        , div []
+            [
+              input [ onInput (Lon >> CompanyAdd), type' "text", placeholder "lon" ] []
+           ]
+        , button [ onClick (CompanyAddPress |> CompanyAdd), type' "text" ] [ text "Add company" ]
+        ]
